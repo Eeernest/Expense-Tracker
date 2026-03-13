22b434c9ad2cd98a576b1510a9dd79899a1ecd42
Eeernest <ernestgrabski16@wp.pl>
2026-03-13 18:23:09 +0100

    feat(permit) add CurrenntUserDep for permit_dependency.py

--- app/dependencies/permit_dependency.py
+++ app/dependencies/permit_dependency.py
@@ -4,6 +4,7 @@ from typing import Annotated
 from app.db.database import SessionDep
 from app.core.security import Security
 from app.core.config import Config
+from app.models.user_model import User
 from app.repositories.permit_repository import PermitRepository
 from app.services.permit_service import PermitService
 
@@ -15,3 +16,10 @@ def get_permit_service(session: SessionDep):
   return PermitService(secure, config, repo)
 
 PermitDep = Annotated[PermitService, Depends(get_permit_service)]
+
+TokenDep = Annotated[str, Depends(Security.oauth2_scheme)]
+
+def current_user(token: TokenDep, service: PermitDep) -> User:
+  return service.get_current_user(token)
+
+CurrentUserDep = Annotated[User, Depends(current_user)]
