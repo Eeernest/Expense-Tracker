3d2576d8a19268a73af106c1e2f6506dc5915729
Eeernest <ernestgrabski16@wp.pl>
2026-03-13 21:32:08 +0100

    test(expense): add success, offset, limit tests for view_all method

--- tests/test_repositories/test_expense_repository.py
+++ tests/test_repositories/test_expense_repository.py
@@ -1,7 +1,7 @@
 from tests.fixtures.expense_fixture import expense_repo, expense_data
 from tests.test_database import db_session
 
-from app.models.expense_model import ExpenseCategory
+from app.models.expense_model import Expense, ExpenseCategory
 
 def test_add_expense_success(expense_repo, expense_data):
   created_expense = expense_repo.add_expense(expense_data)
@@ -9,3 +9,48 @@ def test_add_expense_success(expense_repo, expense_data):
   assert created_expense.id is not None
   assert created_expense.description == "rent"
   assert created_expense.category == ExpenseCategory.housing
+
+def test_view_all(expense_repo):
+  for i in range(6):
+    list = expense_repo.add_expense(
+      Expense(
+        description="rent",
+        amount=1000,
+        category=ExpenseCategory.housing,
+        user_id=1
+      )
+    )
+
+  expense_list = expense_repo.view_all(list.user_id, 0, 10)
+
+  assert len(expense_list) == 6
+
+def test_view_all_offset(expense_repo):
+  for i in range(6):
+    list = expense_repo.add_expense(
+      Expense(
+        description="rent",
+        amount=1000,
+        category=ExpenseCategory.housing,
+        user_id=1
+      )
+    )
+
+  expense_list = expense_repo.view_all(list.user_id, 2, 10)
+
+  assert len(expense_list) == 4
+
+def test_view_all_limit(expense_repo):
+  for i in range(6):
+    list = expense_repo.add_expense(
+      Expense(
+        description="rent",
+        amount=1000,
+        category=ExpenseCategory.housing,
+        user_id=1
+      )
+    )
+
+  expense_list = expense_repo.view_all(list.user_id, 0, 3)
+
+  assert len(expense_list) == 3
