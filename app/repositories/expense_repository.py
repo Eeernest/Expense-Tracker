623cda812f45c2aba8821d2015e5ac1d7e82feff
Eeernest <ernestgrabski16@wp.pl>
2026-03-13 21:29:21 +0100

    feat(expense): add view_all method to ExpenseRepository class

--- app/repositories/expense_repository.py
+++ app/repositories/expense_repository.py
@@ -1,3 +1,4 @@
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 from app.models.expense_model import Expense
 
@@ -11,3 +12,6 @@ class ExpenseRepository:
     self.session.refresh(expense)
 
     return expense
+
+  def view_all(self, user_id: int, offset: int, limit: int) -> list[Expense]:
+    return self.session.execute(select(Expense).where(Expense.user_id == user_id).offset(offset).limit(limit)).scalars().all()
