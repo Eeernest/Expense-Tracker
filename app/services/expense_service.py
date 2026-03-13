19784491d260a8665f9e85e1cf218ae40f67aee3
Eeernest <ernestgrabski16@wp.pl>
2026-03-13 18:15:52 +0100

    refactor(expense) replace cerate for base schema and update add category argument for add_expense method

--- app/services/expense_service.py
+++ app/services/expense_service.py
@@ -1,17 +1,25 @@
 from app.models.expense_model import Expense, ExpenseCategory
 from app.models.user_model import User
-from app.schemas.expense_schema import ExpenseCreate
+from app.schemas.expense_schema import ExpenseBase
 from app.repositories.expense_repository import ExpenseRepository
+
+from fastapi import HTTPException
 
 class ExpenseService:
   def __init__(self, repo: ExpenseRepository):
     self.repo = repo
 
-  def add_expense(self, exp_data: ExpenseCreate, user: User) -> Expense:
+  def add_expense(self, category: ExpenseCategory, exp_data: ExpenseBase, user: User) -> Expense:
+    if exp_data.description is None:
+      raise HTTPException(status_code=422, detail="Description is required")
+    
+    if exp_data.amount is None:
+      raise HTTPException(status_code=422, detail="Amount is required")
+
     new_expense = Expense(
       description=exp_data.description,
       amount=exp_data.amount,
-      category=exp_data.category,
+      category=category,
       user_id=user.id
     )
 
