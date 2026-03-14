8f03889da160b538e131787295af253b9a3ed5ad
Eeernest <ernestgrabski16@wp.pl>
2026-03-14 19:12:23 +0100

    feat(expense): add router for view all feature

--- app/routers/expense_router.py
+++ app/routers/expense_router.py
@@ -1,13 +1,15 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 from app.models.expense_model import ExpenseCategory
 from app.schemas.expense_schema import ExpenseBase, ExpenseRead
 from app.dependencies.expense_dependency import ExpenseDep
 
 from app.dependencies.permit_dependency import CurrentUserDep
 
+from typing import Annotated
+
 router = APIRouter()
 
-@router.post("/expense/", response_model=ExpenseRead)
+@router.post("/expenses/", response_model=ExpenseRead)
 def add_expense(
   category: ExpenseCategory,
   expense: ExpenseBase,
@@ -15,3 +17,12 @@ def add_expense(
   service: ExpenseDep
 ):
   return service.add_expense(category, expense, user)
+
+@router.get("/expenses/", response_model=list[ExpenseRead])
+def view_all(
+  service: ExpenseDep,
+  user_id: CurrentUserDep,
+  offset: int = 0,
+  limit: Annotated[int, Query(le=100)] = 100
+):
+  return service.view_all(user_id, offset, limit)
