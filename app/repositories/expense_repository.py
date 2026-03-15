6bca053bdbd9254b4689581b0451fa1d4a9522c1
Eeernest <ernestgrabski16@wp.pl>
2026-03-15 23:11:17 +0100

    feat(expense): add view_data method and filter thhru category in view_all method for ExpenseRepository class

--- app/repositories/expense_repository.py
+++ app/repositories/expense_repository.py
@@ -1,6 +1,9 @@
 from sqlalchemy import select
 from sqlalchemy.orm import Session
-from app.models.expense_model import Expense
+from app.models.expense_model import Expense, ExpenseCategory
+from app.schemas.expense_schema import ExpenseDate
+
+from datetime import datetime
 
 class ExpenseRepository:
   def __init__(self, session: Session):
@@ -13,5 +16,18 @@ class ExpenseRepository:
 
     return expense
 
-  def view_all(self, user_id: int, offset: int, limit: int) -> list[Expense]:
-    return self.session.execute(select(Expense).where(Expense.user_id == user_id).offset(offset).limit(limit)).scalars().all()
+  def view_all(self, user_id: int, offset: int, limit: int, category: ExpenseCategory | None = None) -> list[Expense]:
+    statement = select(Expense).where(Expense.user_id == user_id).offset(offset).limit(limit)
+
+    if category:
+      statement = statement.where(Expense.category == category)
+
+    return self.session.execute(statement).scalars().all()
+  
+  def view_date(self, user_id: int, start_date: datetime, end_date: datetime, offset: int, limit: int, category: ExpenseCategory | None = None) -> list[Expense]:
+    statement = select(Expense).where(Expense.user_id == user_id, Expense.date >= start_date, Expense.date < end_date).offset(offset).limit(limit)
+
+    if category:
+      statement = statement.where(Expense.category == category)
+
+    return self.session.execute(statement).scalars().all()
