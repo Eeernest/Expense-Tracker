a0e5d6a44a3d68d877b4756127d5efc9581fad44
Eeernest <ernestgrabski16@wp.pl>
2026-03-16 11:31:49 +0100

    test(expense): add test for filtering out expenses thru category and refactor offset and limit values in tests for view_all method

--- tests/repositories/test_expense_repository.py
+++ tests/repositories/test_expense_repository.py
@@ -14,13 +14,24 @@ def test_view_all_success(expense_repo, expense_list):
   user_id = 1
   lists = expense_repo.view_all(user_id, 0, 10)
 
-  assert len(lists) == 6
+  assert len(lists) == 10
+  assert all(exp.user_id == user_id for exp in lists)
+
+def test_view_all_category(expense_repo, expense_list):
+  user_id = 1
+  category = ExpenseCategory.housing
+
+  lists = expense_repo.view_all(user_id, 0, 10, category)
+
+  assert len(lists) == 5
+  assert all(exp.user_id == user_id for exp in lists)
+  assert all(exp.category == ExpenseCategory.housing for exp in lists)
 
 def test_view_all_offset(expense_repo, expense_list):
   user_id = 1
   lists = expense_repo.view_all(user_id, 2, 10)
 
-  assert len(lists) == 4
+  assert len(lists) == 8
 
 def test_view_all_limit(expense_repo, expense_list):
   user_id = 1
