494a1f411ae77f15bee0156b4ce08130d0b0362c
Eeernest <ernestgrabski16@wp.pl>
2026-03-16 17:00:35 +0100

    refactor(expense): replace user_id for just a number as argument in expense_repo forview all and date method tests

--- tests/repositories/test_expense_repository.py
+++ tests/repositories/test_expense_repository.py
@@ -13,81 +13,72 @@ def test_add_expense_success(expense_repo, expense_data):
   assert created_expense.category == ExpenseCategory.housing
 
 def test_view_all_success(expense_repo, expense_list):
-  user_id = 1
-  lists = expense_repo.view_all(user_id, 0, 10)
+  lists = expense_repo.view_all(1, 0, 10)
 
   assert len(lists) == 10
-  assert all(exp.user_id == user_id for exp in lists)
+  assert all(exp.user_id == 1 for exp in lists)
 
 def test_view_all_category(expense_repo, expense_list):
-  user_id = 1
   category = ExpenseCategory.entertainment
 
-  lists = expense_repo.view_all(user_id, 0, 10, category)
+  lists = expense_repo.view_all(1, 0, 10, category)
 
   assert len(lists) == 5
-  assert all(exp.user_id == user_id for exp in lists)
+  assert all(exp.user_id == 1 for exp in lists)
   assert all(exp.category == ExpenseCategory.entertainment for exp in lists)
   assert all(exp.category != ExpenseCategory.housing for exp in lists)
 
 def test_view_all_offset(expense_repo, expense_list):
-  user_id = 1
-  lists = expense_repo.view_all(user_id, 2, 10)
+  lists = expense_repo.view_all(1, 2, 10)
 
   assert len(lists) == 8
 
 def test_view_all_limit(expense_repo, expense_list):
-  user_id = 1
-  lists = expense_repo.view_all(user_id, 0, 3)
+  lists = expense_repo.view_all(1, 0, 3)
 
   assert len(lists) == 3
 
 def test_view_date_ninety_days(expense_repo, expense_dates):
-  user_id = 1
   end_date = datetime(2026, 3, 1)
   start_date = end_date - timedelta(days=90)
 
-  lists = expense_repo.view_date(user_id, start_date, end_date, 0, 100)
+  lists = expense_repo.view_date(1, start_date, end_date, 0, 100)
 
   assert len(lists) == 20
 
 def test_view_date_thirty_days(expense_repo, expense_dates):
-  user_id = 1
   end_date = datetime(2026, 3, 1)
   start_date = end_date - timedelta(days=30)
 
-  lists = expense_repo.view_date(user_id, start_date, end_date, 0, 100)
+  lists = expense_repo.view_date(1, start_date, end_date, 0, 100)
 
   assert len(lists) == 15
 
 def test_view_date_seven_days(expense_repo, expense_dates):
-  user_id = 1
   end_date = datetime(2026, 3, 1)
   start_date = end_date - timedelta(days=7)
 
-  lists = expense_repo.view_date(user_id, start_date, end_date, 0, 100)
+  lists = expense_repo.view_date(1, start_date, end_date, 0, 100)
 
   assert len(lists) == 10
 
 def test_view_date_today(expense_repo, expense_dates):
-  user_id = 1
   end_date = datetime(2026, 3, 1)
   start_date = end_date
 
-  lists = expense_repo.view_date(user_id, start_date, end_date, 0, 100)
+  lists = expense_repo.view_date(1, start_date, end_date, 0, 100)
 
   assert len(lists) == 5
 
 def test_view_date_category(expense_repo, expense_dates):
-  user_id = 1
   end_date = datetime(2026, 3, 1)
   start_date = end_date - timedelta(days=90)
   category = ExpenseCategory.healthcare
 
-  lists = expense_repo.view_date(user_id, start_date, end_date, 0, 100, category)
+  lists = expense_repo.view_date(1, start_date, end_date, 0, 100, category)
 
   assert len(lists) == 5
-  assert all(exp.user_id == user_id for exp in lists)
+  assert all(exp.user_id == 1 for exp in lists)
   assert all(exp.category == ExpenseCategory.healthcare for exp in lists)
   assert all(exp.category != ExpenseCategory.housing for exp in lists)
   assert all(exp.category != ExpenseCategory.food for exp in lists)
