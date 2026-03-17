399791e10d882dc1b4c009b28bcc63dd2a731297
Eeernest <ernestgrabski16@wp.pl>
2026-03-17 12:01:12 +0100

    refactor(auth): replace found_user string name with result name and delete user variable in auth repository test

--- tests/repositories/test_auth_repository.py
+++ tests/repositories/test_auth_repository.py
@@ -2,8 +2,7 @@ from tests.test_database import db_session
 from tests.fixtures.auth_fixture import auth_repo, user_data, created_user
 
 def test_check_username(auth_repo, created_user):
-  user = created_user
-  found_user = auth_repo.check_username(user.username)
+  result = auth_repo.check_username(created_user.username)
 
-  assert found_user is not None
-  assert found_user.username == "user1"
+  assert result is not None
+  assert result.username == "user1"
