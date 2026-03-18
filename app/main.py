477fcf9bd6cc1fc133434645d29c9043b44f1c1c
Eeernest <ernestgrabski16@wp.pl>
2026-03-18 21:50:44 +0100

    feat(user): add function that creates first admin

--- app/main.py
+++ app/main.py
@@ -1,17 +1,29 @@
 from fastapi import FastAPI
 
-from app.db.database import create_db_and_table
+from app.db.database import create_db_and_table, SessionLocal
 from app.routers.user_router import router as user_router
 from app.routers.auth_router import router as auth_router
 from app.routers.permit_router import router as permit_router
 from app.routers.expense_router import router as expense_router
 
-app = FastAPI()
+from app.core.security import Security
+from app.core.config import Config
+from app.repositories.user_repository import UserRepository
+from app.services.user_service import UserService
 
+app = FastAPI()
 
 @app.on_event("startup")
 def on_startup():
   create_db_and_table()
+
+  db = SessionLocal()
+  try:
+    repo = UserRepository(db)
+    service = UserService(Security(), Config(), repo)
+    service.create_admin()
+  finally:
+    db.close()
 
 @app.get("/")
 def read_root():
