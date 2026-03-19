f8e91bd26a4fb7e91c4f8b42256ef2f8fdc9d1fc
Eeernest <ernestgrabski16@wp.pl>
2026-03-19 12:40:59 +0100

    test(permit): add tests for get_current_admin method from PermitService class

--- tests/services/test_permit_service.py
+++ tests/services/test_permit_service.py
@@ -5,6 +5,8 @@ from jwt.exceptions import InvalidTokenError
 from tests.fixtures.permit_fixture import secure, config, repo, user_data, created_user, permit_service
 from tests.test_database import db_session
 
+from app.models.user_model import UserRole
+
 def test_get_current_user_success(secure, repo, created_user, permit_service):
   token = "valid token"
 
@@ -55,6 +57,35 @@ def test_user_not_found(secure, repo, permit_service):
 
   assert exc.value.status_code == 401
   assert "Could" in str(exc.value)
+
+  secure.decode_jwt.assert_called_once()
+  repo.check_user_id.assert_called_once()
+
+def test_get_current_admin_success(secure, repo, created_user, permit_service):
+  token = "valid token"
+  created_user.role = UserRole.admin
+
+  secure.decode_jwt.return_value = {"sub": str(created_user.id), "role": created_user.role}
+  repo.check_user_id.return_value = created_user
+
+  result = permit_service.get_current_admin(token)
+
+  assert result == created_user
+
+  secure.decode_jwt.assert_called_once()
+  repo.check_user_id.assert_called_once()
+
+def test_get_suer_token_no_permission(secure, repo, created_user, permit_service):
+  token = "valid token"
+
+  secure.decode_jwt.return_value = {"sub": str(created_user.id), "role": created_user.role}
+  repo.check_user_id.return_value = created_user
+
+  with pytest.raises(HTTPException) as exc:
+    permit_service.get_current_admin(token)
+
+  assert exc.value.status_code == 403
+  assert exc.value.detail == "Not enough permission"
 
   secure.decode_jwt.assert_called_once()
   repo.check_user_id.assert_called_once()
